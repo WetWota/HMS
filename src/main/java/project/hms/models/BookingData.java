c350fc9d9aba6bde7572bdfd29a5f1d1ee09f947
WetWota <82423896+WetWota@users.noreply.github.com>
2025-03-18 17:33:11 +0800

    1.5
    booking update

---
+++ src/main/java/project/hms/models/BookingData.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.hms.models;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author wkbui
+ */
+public class BookingData {
+    private int bookingID;
+    private int patientID;
+    private int staffID;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    private String status;
+    
+    public BookingData(int bookingID, int patientID, int staffID, LocalDate appointmentDate, LocalTime appointmentTime, String status){
+        this.bookingID = bookingID;
+        this.patientID = patientID;
+        this.staffID = staffID;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.status = status;
+    }
+    
+    public int getBookingID(){ return bookingID; }
+    public int getPatientID() { return patientID; }
+    public int getStaffID() { return staffID; }
+    public LocalDate getAppointmentData() { return appointmentDate; }
+    public LocalTime getAppointmentTime() { return appointmentTime; }
+    public String status() { return status; }
+    
+    public void setBookingID() { this.bookingID = bookingID; }
+    public void setPatientID() { this.patientID = patientID; }
+    public void setStaffID() { this.staffID = staffID; }
+}
